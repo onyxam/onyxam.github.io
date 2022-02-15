949d2e89961c5188ee3bdaba243fecb417da79e6
onyxam <onyxamy@gmail.com>
2022-02-15 17:58:50 +0200

    polish

---
+++ website/Scripts/app.js
@@ -0,0 +1,16 @@
+
+function Home() {
+    window.location.href = "https://onyxam.github.io/";
+}
+
+function About() {
+    window.location.href = "https://onyxam.github.io/about";
+}
+
+function GitHub() {
+    window.location.href = "https://github.com/onyxam";
+}
+
+function YouTube() {
+    window.location.href = "https://www.youtube.com/channel/UCgOuuyZ2QyfPONsJLlTrl1w";
+}
